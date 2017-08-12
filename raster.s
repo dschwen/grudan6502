96e99e255f48b69d7b0b417eecd858844f8cdc62
Daniel Schwen <daniel@schwen.de>
2017-08-12 15:44:01 -0600

    getting closer

--- raster.s
+++ raster.s
@@ -14,7 +14,9 @@
 ;.endif
 ;	sta $D010
 ;.endmacro
-;
+
+xoffset = 148
+
 ; load image
 	lda #$42	; 'B'
 	sta $22
@@ -27,23 +29,23 @@
 	lda #%11111111
 	sta $D015
 ; set x positions
-	lda #100
+	lda #<(xoffset)
 	sta $D000
-	lda #124
+	lda #<(xoffset + 24)
 	sta $D002
-	lda #148
+	lda #<(xoffset + 24 * 2)
 	sta $D004
-	lda #172
+	lda #<(xoffset + 24 * 3)
 	sta $D006
-	lda #196
+	lda #<(xoffset + 24 * 4)
 	sta $D008
-	lda #220
+	lda #<(xoffset + 24 * 5)
 	sta $D00a
-	lda #244
+	lda #<(xoffset + 24 * 6)
 	sta $D00c
-	lda #13
+	lda #<(xoffset + 24 * 7)
 	sta $D00e
-	lda #%10000000
+	lda #%11100000
 	sta $D010
 ; set sprite colors
 	ldx #8
@@ -71,29 +73,12 @@
 ; return to basic
 	rts
 
-yoffset = 50 ; 56
-yheight = 20
+yoffset = 54 ; 50
+yheight = 21
 
 .macro	movesprites pos, irq, base
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	lda #2
-	sta $D021
-; set sprite positions
-	lda pos
-	sta $D001
-	sta $D003
-	sta $D005
-	sta $D007
-	sta $D009
-	sta $D00B
-	sta $D00D
-	sta $D00F
+	;lda #2
+	;sta $D021
 ; set sprite pointers
 	ldx base
 	stx $07F8
@@ -111,43 +96,53 @@ yheight = 20
 	stx $07FE
 	ldx base+7
 	stx $07FF
+; set sprite positions
+	lda #pos
+	sta $D001
+	sta $D003
+	sta $D005
+	sta $D007
+	sta $D009
+	sta $D00B
+	sta $D00D
+	sta $D00F
 ; set raster line number
-	lda pos + yheight - 1
+	lda #(pos-1)
 	sta $D012
 	lda #<irq
 	sta $0314
 	lda #>irq
 	sta $0315
 	asl $d019
-	lda #6
-	sta $D021
+	;lda #6
+	;sta $D021
 .endmacro
 
-irq1:	movesprites #(yoffset), irq2, #184
+irq1:	movesprites (yoffset + yheight), irq2, #184
 	jmp $EA81
 
-irq2:	movesprites #(yoffset + yheight), irq3, #184+8
+irq2:	movesprites (yoffset + yheight * 2), irq3, #184+8
 	jmp $EA81
 
-irq3:	movesprites #(yoffset + yheight * 2), irq4, #184+16
+irq3:	movesprites (yoffset + yheight * 3), irq4, #184+16
 	jmp $EA81
 
-irq4:	movesprites #(yoffset + yheight * 3), irq5, #184+24
+irq4:	movesprites (yoffset + yheight * 4), irq5, #184+24
 	jmp $EA81
 
-irq5:	movesprites #(yoffset + yheight * 4), irq6, #184+32
+irq5:	movesprites (yoffset + yheight * 5), irq6, #184+32
 	jmp $EA81
 
-irq6:	movesprites #(yoffset + yheight * 5), irq7, #184+40
+irq6:	movesprites (yoffset + yheight * 6), irq7, #184+40
 	jmp $EA81
 
-irq7:	movesprites #(yoffset + yheight * 6), irq8, #184+48
+irq7:	movesprites (yoffset + yheight * 7), irq8, #184+48
 	jmp $EA81
 
-irq8:	movesprites #(yoffset + yheight * 7), irq9, #184+56
+irq8:	movesprites (yoffset + yheight * 8), irq9, #184+56
 	jmp $EA81
 
-irq9:	movesprites #(yoffset + yheight * 8), irq1, #184+64
+irq9:	movesprites yoffset, irq1, #184+64
 	jmp $EA31
 
 
